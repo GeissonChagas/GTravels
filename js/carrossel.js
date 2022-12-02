5cfa7e48d0a85427856eaa235109261a0c0ac7d7
GeissonChagas <geisson.excel@gmail.com>
2022-12-02 11:07:38 -0300

    melhorando carrossel

--- js/carrossel.js
+++ js/carrossel.js
@@ -1,21 +1,9 @@
+
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+spaceBetween: 10,
+slidesPerView: 3,
+pagination: {
+el: '.swiper-pagination',
+type: 'bullets',
+},
+});
